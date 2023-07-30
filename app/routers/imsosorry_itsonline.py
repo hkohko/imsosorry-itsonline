ef6463db11a51a1f56b971845be5f450c56c8b3b
heko <91366057+hkohko@users.noreply.github.com>
2023-07-30 16:05:44 +0700

    randomly select strength on a range

--- app/routers/imsosorry_itsonline.py
+++ app/routers/imsosorry_itsonline.py
@@ -1,13 +1,31 @@
 from fastapi import APIRouter
 from imsosorry import uwuify
 from pydantic import BaseModel
+from random import random
 
-class Uwu(BaseModel):
+
+class UwU(BaseModel):
     text: str
+    cultured: str = "medium"
+
 
 router = APIRouter()
 
+
 @router.post("/uwuify")
-async def ancient_scrolls(text: Uwu):
-    uwuified = uwuify(text.text)
-    return uwuified
+async def ancient_scrolls(text: UwU, culture: str):
+    params = {
+        "high": random(0.7, 1.0),
+        "medium": random(0.4, 0.7),
+        "low": random(0.1, 0.4),
+    }
+    if culture in ("high", "medium", "low"):
+        uwuified = uwuify(
+            text.text,
+            stutter_strength=params.get(culture),
+            emoji_strength=params.get(culture),
+            tilde_strength=params.get(culture),
+        )
+        return uwuified
+    else:
+        return {"Error": {"valid strengths": ["high", "medium", "low"]}}
